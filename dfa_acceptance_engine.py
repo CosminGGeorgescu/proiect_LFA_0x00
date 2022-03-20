8d0b71058adcc7274cad649b24d6a81234f15244
CosminGGeorgescu <95082750+CosminGGeorgescu@users.noreply.github.com>
2022-03-20 15:32:59 +0200

    Added support for strings

--- dfa_acceptance_engine.py
+++ dfa_acceptance_engine.py
@@ -30,17 +30,21 @@ while line:=f.readline():   #cat timp mai sunt linii de citit
             t.append(line[0].split('state')[1])
             t.append(line[1].split('letter')[1])
             t.append(line[2].split('state')[1].split('\n')[0])
-            if (t[0] not in states) or (t[2] not in states) or (t[1] not in alphabet):
-                exit("INVALID DFA")     #tratez validitatea starilor si a literei
             if (t[0], t[2]) in transitions:
                 transitions[(t[0], t[2])].append(t[1])    
             else:
                 transitions[(t[0], t[2])]=[t[1]]    #pentru formatul din pdf, t[0] si t[2] sunt stari, iar t[1] e litera din alfabet
-for (x, y) in transitions:
-    for cost1 in transitions[(x, y)]: #pentru toate combinatiile (x, y) existente caut (x, z) valid cu valoare muchiei(x, y)= valoarea muchiei (x, z)
-        for z in states:
-            if (x, z) in transitions and y!=z:
-                for cost2 in transitions[(x, z)]:
-                    if cost1==cost2: #daca exista o pereche (x, y, z) care satisface conditiile de mai sus, DFA-ul e nedeterminist
-                        exit("INVALID DFA")
-exit("VALID DFA")      #daca ajunge pana la sfarsit fara sa dea de celelalte stopuri, e valid
+input=sys.argv[2]
+i=0
+state=S
+while i<len(input):
+    for x in states:
+        if (state, x) in transitions:
+            for cost in transitions[(state, x)]:
+                if cost==input[i]:
+                    state=x
+                    break
+    i=i+1
+if state in F:
+    exit("VALID INPUT")
+exit("INVALID INPUT")
