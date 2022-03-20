ffda3d4ad8984228a319da659accfccbb648d387
CosminGGeorgescu <95082750+CosminGGeorgescu@users.noreply.github.com>
2022-03-20 13:53:38 +0200

    Update dfa_acceptance_engine.py

--- dfa_acceptance_engine.py
+++ dfa_acceptance_engine.py
@@ -2,45 +2,46 @@ import sys
 f=open(sys.argv[1], 'r')
 states=[]
 alphabet=[]
+S=0
 F=[]
-transitions={}      #pentru explicatii la citire, vezi "validare AFD"
-while line:=f.readline():
-    if "Sigma" in line:
+transitions={}
+#rezolva acceptarea si a char-urilor la States si Sigma
+while line:=f.readline():   #cat timp mai sunt linii de citit
+    if "Sigma" in line:    #daca pe linie e Sigma atunci incepe citirea alfabetului
         while "End" not in (line:=f.readline()):
-            alphabet.append(int(line.split('letter')[1][0]))
-    elif "States" in line:
+            alphabet.append(line.split('letter')[1].split('\n')[0])
+    elif "States" in line:    #daca pe linie e States atunci incepe citirea starilor
         while "End" not in (line:=f.readline()):
-            if ("S" in line) and ("F" in line):
-                S=int(line.split('state')[1].split()[0])
-                states.append(S)
+            if ("S" in line) and ("F" in line):    #trateaza cazul cand pe linie e si S si F
+                S=line.split('state')[1].split(' ,S ,F')[0]
                 F.append(S)
-            elif "S" in line:
-                S=int(line.split('state')[1].split()[0])
                 states.append(S)
-            elif "F" in line:
-                F.append(int(line.split('state')[1].split()[0]))
-                states.append(int(line.split('state')[1].split()[0]))
+            elif "S" in line:   #trateaza cazul cand starea e cea initiala
+                S=line.split('state')[1].split(' ,S')[0]
+                states.append(S)
+            elif "F" in line:   #trateaza cazul cand starea e finala
+                F.append(line.split('state')[1].split(' ,F')[0])
+                states.append(F[len(F)-1])
             else:
-                states.append(int(line.split('state')[1]))
-    elif "Transitions" in line:
-        while "End" not in (line:=f.readline()):
-            line=line.split(',')
+                states.append(line.split('state')[1].split('\n')[0])
+    elif "Transitions" in line:   #daca pe linie e Transitions, incepe citirea tranzitiilor de la linia urmatoare
+        while "End" not in (line:=f.readline()):    #citim pana dam de End pe linie
+            line=line.split(' ,')
             t=[]
-            for x in line:
-                t.append(int(x[len(x)-2]))
-            transitions[(t[0], t[2])]=t[1]
-input=sys.argv[2] #inputul de validat
-i=S     #incepem de la inceput
-for x in input:  #pentru fiecare litera a inputului
-    j=0; s=0
-    while j<len(states) and s==0:   #pentru input[x], caut pereche (i, j) cu valoarea muchiei=input[x]
-        if ((i, j) in transitions) and transitions[(i, j)]==int(x):
-            i=j
-            s=1     #variabla kill switch
-        j=j+1
-    if s==0:  #la prima litera pentru care nu gaseste pereche (i, j) care sa satisfaca conditiile de mai sus, input invalid
-        exit("INVALID INPUT")
-if i in F:
-    exit("VALID INPUT")
-else:  #daca starea la care se opreste validarea inputului nu e finala, input invalid
-    exit("INVALID INPUT")
+            t.append(line[0].split('state')[1])
+            t.append(line[1].split('letter')[1])
+            t.append(line[2].split('state')[1].split('\n')[0])
+            if (t[0] not in states) or (t[2] not in states) or (t[1] not in alphabet):
+                exit("INVALID DFA")     #tratez validitatea starilor si a literei
+            if (t[0], t[2]) in transitions:
+                transitions[(t[0], t[2])].append(t[1])    
+            else:
+                transitions[(t[0], t[2])]=[t[1]]    #pentru formatul din pdf, t[0] si t[2] sunt stari, iar t[1] e litera din alfabet
+for (x, y) in transitions:
+    for cost1 in transitions[(x, y)]: #pentru toate combinatiile (x, y) existente caut (x, z) valid cu valoare muchiei(x, y)= valoarea muchiei (x, z)
+        for z in states:
+            if (x, z) in transitions and y!=z:
+                for cost2 in transitions[(x, z)]:
+                    if cost1==cost2: #daca exista o pereche (x, y, z) care satisface conditiile de mai sus, DFA-ul e nedeterminist
+                        exit("INVALID DFA")
+exit("VALID DFA")      #daca ajunge pana la sfarsit fara sa dea de celelalte stopuri, e valid
